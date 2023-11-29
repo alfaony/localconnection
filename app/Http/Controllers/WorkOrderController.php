c1cfa5e9d7a217656c27f5563fc76ab973dcf06b
alfaony <alfaony@gmail.com>
2023-11-29 14:23:55 +0700

    sort suggetion

--- app/Http/Controllers/WorkOrderController.php
+++ app/Http/Controllers/WorkOrderController.php
@@ -147,13 +147,14 @@ class WorkOrderController extends Controller
         $quote = Quote::find($id);
         $quoteProduct = $quote->quoteProduct 
         ? $quote->quoteProduct()
-                ->select('product_id', 'qty', 'description')
+                ->select('product_id', 'qty', 'description', 'sort')
+                ->orderBy('sort')
                 ->get()
                 ->map(function($item) {
                     return [
                         'product_id' => $item->product_id,
                         'qty' => $item->qty,
-                        'description' => $item->description
+                        'description' => $item->description,
                     ];
                 })
         : collect();
