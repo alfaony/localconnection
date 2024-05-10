b4d22ad9eea761dee2dc9b21d8cb17fe9b0b0003
alfaony <alfaony@gmail.com>
2024-05-10 16:02:38 +0700

    stop js

--- resources/views/asset/index.blade.php
+++ resources/views/asset/index.blade.php
@@ -127,6 +127,7 @@
 @section('js')
 <script type="text/javascript" src="https://cdn.jsdelivr.net/jquery/latest/jquery.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+@stop
 
 @section('css')
     <style>
