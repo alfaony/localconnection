be8b72be940bd3a09e514b60246d7bfade40f311
alfaony <alfaony@gmail.com>
2024-06-18 09:49:04 +0700

    update asessor

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -88,18 +88,26 @@ class Project extends Model
 
     public function getProgressPercentageAttribute()
     {
-        $now = Carbon::now();
-        $start_date = Carbon::parse($this->start_date);
-        $end_date = Carbon::parse($this->end_date);
+        if($this->start_date && $this->end_date)
+        {
+            $now = Carbon::now();
+            $start_date = Carbon::parse($this->start_date);
+            $end_date = Carbon::parse($this->end_date);
 
-        if ($now->greaterThanOrEqualTo($end_date)) {
-            return 100;
+            if ($now->greaterThanOrEqualTo($end_date)) {
+                return 100;
+            }
+    
+            $totalDuration = $start_date->diffInSeconds($end_date);
+            $elapsedDuration = $start_date->diffInSeconds($now);
+    
+            return $totalDuration > 0 ? intval(($elapsedDuration / $totalDuration) * 100) : 0;
+
+        }else
+        {
+            return 0;
         }
 
-        $totalDuration = $start_date->diffInSeconds($end_date);
-        $elapsedDuration = $start_date->diffInSeconds($now);
-
-        return $totalDuration > 0 ? round(($elapsedDuration / $totalDuration) * 100, 2) : 0;
     }
     
     public function workOrder()
