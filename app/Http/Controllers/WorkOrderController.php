efb8dc28b286c0e4643b5048ec27c4b28f2d119c
alfaony <alfaony@gmail.com>
2024-06-21 16:02:21 +0700

    fixing

--- app/Http/Controllers/WorkOrderController.php
+++ app/Http/Controllers/WorkOrderController.php
@@ -109,7 +109,6 @@ class WorkOrderController extends Controller
             //throw $th;
             DB::rollback();
             Log::error($th);
-            dd($th);
             // dd($th);
             return redirect()->to(route('work-order.index'))->with('store',false);
         }
