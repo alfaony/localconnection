664de847fef99cad2ce1ec592dac70b914013f56
AlfaonyThrive <alfaony@thrive.co.id>
2024-08-23 10:30:27 +0700

    Add Kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,6 +25,7 @@ class Kernel extends ConsoleKernel
         // Jadwalkan pekerjaan 'project:reccuring' setiap hari pada pukul 00:00
         $schedule->command('project:reccuring')->timezone('Asia/Jakarta')->dailyAt('00:00');
         $schedule->command('project:set-status-sent-time')->timezone('Asia/Jakarta')->dailyAt('00:00');
+        $schedule->command('tasks:process-recurring')->timezone('Asia/Jakarta')->dailyAt('00:00');
 
         $company = Company::all();
         foreach ($company as $a) 
