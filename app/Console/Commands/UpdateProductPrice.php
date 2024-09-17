36ca1a385788f91d5c12a5d1c00de63e332c2f30
AlfaonyThrive <alfaony@thrive.co.id>
2024-09-17 08:33:19 +0700

    Product Price

---
+++ app/Console/Commands/UpdateProductPrice.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Product;
+use App\Models\ProductCategory;
+use App\Models\Company;
+use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Facades\Excel;
+
+use App\Models\Role;
+use App\Models\User;
+
+use App\Schemas\RoleSchema;
+class UpdateProductPrice extends Command
+{
+    protected $signature = 'update:product-price {file}';
+    protected $description = 'Update product price from an Excel file';
+
+    public function handle()
+    {
+        $filePath = resource_path('files/' . $this->argument('file'));
+        
+        $data = Excel::toArray([], $filePath)[0];
+
+        DB::transaction(function () use ($data) {
+            foreach ($data as $row) {
+                $productName = $row[0];
+                $companyName = $row[1];
+                $priceBuy = $row[2];
+                $priceSell = $row[3];
+
+                $company = Company::where('name',$companyName)->first();
+
+                $product = Product::byCompany($company->id)->where('name', 'LIKE', "%{$productName}%")->first();
+                $product->price_buy = $priceBuy;
+                $product->price_sell = $priceSell;
+                $product->save();
+            }
+        });
+
+        $this->info('Product Price updated successfully.');
+    }
+}
