d3ccdea2b7818899ba9600eefb881ccab3673573
AlfaonyThrive <alfaony@thrive.co.id>
2024-09-19 16:49:32 +0700

    Fixing database parameter is wrong

--- resources/views/bast/index.blade.php
+++ resources/views/bast/index.blade.php
@@ -1,7 +1,7 @@
 @extends('adminlte::page')
 
 @section('content_header')
-    <h1>Daftar BAST dan SPK</h1>
+    <h1>Daftar BAST</h1>
 @stop
 
 @section('content')
@@ -33,87 +33,85 @@
     @endif
 </div>
 
-<div class="container">
-    <!-- Nav tabs -->
-    <ul class="nav nav-tabs mb-4" id="myTab" role="tablist">
-        @canAccess('dataTableJson','basts')
-        <li class="nav-item">
-            <a class="nav-link active" id="bast-tab" data-toggle="tab" href="#bast" role="tab" aria-controls="bast" aria-selected="true">
-                <i class="fas fa-file-alt"></i> BAST
-            </a>
-        </li>
-        @endcanAccess
-        @canAccess('dataTableJsonWorkOrderWithoutBast','basts')
-        <li class="nav-item">
-            <a class="nav-link" id="spk-tab" data-toggle="tab" href="#spk_bast" role="tab" aria-controls="spk" aria-selected="false">
-                <i class="fa fa-clipboard-list"></i> SPK ( Belum Terbuat BAST )
-            </a>
-        </li>
-        @endcanAccess
-    </ul>
+<!-- Nav tabs -->
+<ul class="nav nav-tabs mb-4" id="myTab" role="tablist">
+    @canAccess('dataTableJson','basts')
+    <li class="nav-item">
+        <a class="nav-link active" id="bast-tab" data-toggle="tab" href="#bast" role="tab" aria-controls="bast" aria-selected="true">
+            <i class="fas fa-file-alt"></i> BAST
+        </a>
+    </li>
+    @endcanAccess
+    @canAccess('dataTableJsonWorkOrderWithoutBast','basts')
+    <li class="nav-item">
+        <a class="nav-link" id="spk-tab" data-toggle="tab" href="#spk_bast" role="tab" aria-controls="spk" aria-selected="false">
+            <i class="fa fa-clipboard-list"></i> SPK ( Belum Terbuat BAST )
+        </a>
+    </li>
+    @endcanAccess
+</ul>
 
-    <!-- Tab panes -->
-    <div class="tab-content">
-        <!-- BAST Tab -->
-        @canAccess('dataTableJson','basts')
-        <div class="tab-pane fade show active" id="bast" role="tabpanel" aria-labelledby="bast-tab">
-            <div class="card mt-3 shadow-sm">
-                <div class="card-header bg-primary text-white">
-                    <h3 class="card-title">BAST List</h3>
-                    @canAccess('create','basts')
-                    <button class="btn btn-light float-right" id="btnCreateManager">
-                        <i class="fas fa-plus-circle"></i> Tambah BAST
-                    </button>
-                    @endcanAccess
-                </div>
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <table class="table table-bordered table-hover table-striped" id="datatableBast">
-                            <thead class="bg-light">
-                                <tr>
-                                    <th>Nomor BAST</th>
-                                    <th>Tanggal</th>
-                                    <th>Nomor SPK</th>
-                                    <th>Aksi</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <!-- Baris data akan terisi melalui DataTables -->
-                            </tbody>
-                        </table>
-                    </div>
+<!-- Tab panes -->
+<div class="tab-content">
+    <!-- BAST Tab -->
+    @canAccess('dataTableJson','basts')
+    <div class="tab-pane fade show active" id="bast" role="tabpanel" aria-labelledby="bast-tab">
+        <div class="card mt-3 shadow-sm">
+            <div class="card-header bg-primary text-white">
+                <h3 class="card-title">BAST List</h3>
+                @canAccess('create','basts')
+                <button class="btn btn-light float-right" id="btnCreateManager">
+                    <i class="fas fa-plus-circle"></i> Tambah BAST
+                </button>
+                @endcanAccess
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered table-hover table-striped" id="datatableBast">
+                        <thead class="bg-light">
+                            <tr>
+                                <th>Nomor BAST</th>
+                                <th>Tanggal</th>
+                                <th>Nomor SPK</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- Baris data akan terisi melalui DataTables -->
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
-        @endcanAccess
-
-        @canAccess('dataTableJsonWorkOrderWithoutBast','basts')
-        <!-- SPK Tab -->
-        <div class="tab-pane fade" id="spk_bast" role="tabpanel" aria-labelledby="spk-tab">
-            <div class="card mt-3 shadow-sm">
-                <div class="card-header bg-primary text-white">
-                    <h3 class="card-title">List SPK</h3>
-                </div>
-                <div class="card-body">
-                    <div class="table-responsive">
-                        <table class="table table-bordered" id="dataTableJsonWorkOrderWithoutBast" style="width:100%">
-                            <thead>
-                                <tr>
-                                    <th>Nomor SPK</th>
-                                    <th>Total Anggaran</th>
-                                    <th>Aksi</th>
-                                </tr>
-                            </thead>
-                            </tbody>
-                                <!-- ... Tambahkan baris lain sesuai kebutuhan ... -->
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </div>
-        @endcanAccess
     </div>
+    @endcanAccess
+
+    @canAccess('dataTableJsonWorkOrderWithoutBast','basts')
+    <!-- SPK Tab -->
+    <div class="tab-pane fade" id="spk_bast" role="tabpanel" aria-labelledby="spk-tab">
+        <div class="card mt-3 shadow-sm">
+            <div class="card-header bg-primary text-white">
+                <h3 class="card-title">List SPK</h3>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered" id="dataTableJsonWorkOrderWithoutBast" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>Nomor SPK</th>
+                                <th>Total Anggaran</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        </tbody>
+                            <!-- ... Tambahkan baris lain sesuai kebutuhan ... -->
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endcanAccess
 </div>
 
 @stop
