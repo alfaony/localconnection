9799eaddb0467b28c4525b188f804ee0d33b56b1
AlfaonyThrive <alfaony@thrive.co.id>
2024-09-30 10:35:53 +0700

    Update Surat Template

--- resources/views/letter_submission/template/perjanjian_kerja_template.blade.php
+++ resources/views/letter_submission/template/perjanjian_kerja_template.blade.php
@@ -320,17 +320,13 @@
         @csrf
         @method('PATCH')
         <input type="hidden" name="selected_ids[]" value="{{ $letterSubmission->id }}">
-        <div class="form-group">
-            <label for="reason">Alasan Penolakan:</label>
-            <input type="text" name="notes[{{ $letterSubmission->id }}]" class="form-control" value="{{ $letterSubmission->reason ?? '' }}">
-        </div>
         <div class="d-flex justify-content-center">
             <!-- Approve Button -->
             <button type="submit" class="btn btn-success mx-2" name="action" value="approve">
                 <i class="fa fa-check"></i> Approve
             </button>
             <!-- Decline Button -->
-            <button type="submit" class="btn btn-danger mx-2" name="action" value="decline">
+            <button type="button" class="btn btn-danger mx-2" data-bs-toggle="modal" data-bs-target="#declineModal">
                 <i class="fa fa-times"></i> Decline
             </button>
         </div>
