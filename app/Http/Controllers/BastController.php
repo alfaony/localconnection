cb24bcb166b2811e83559bdd4698cf6b039d3da8
AlfaonyThrive <alfaony@thrive.co.id>
2024-10-07 17:16:16 +0700

    sort by

--- app/Http/Controllers/BastController.php
+++ app/Http/Controllers/BastController.php
@@ -195,7 +195,7 @@ class BastController extends Controller
     public function dataTableJson()
     {
         // Fetch data for the DataTable
-        $query = Bast::with('workOrder')->byCompany(Auth::user()->company_id);
+        $query = Bast::with('workOrder')->byCompany(Auth::user()->company_id)->orderBy('date','desc');
 
 
         // Map column indexes to column names (this may vary based on your table structure)
@@ -275,7 +275,7 @@ class BastController extends Controller
             // Filter project yang tidak memiliki reportProject (HasOne)
             $q->doesntHave('bast');
             $q->has('reportProject');
-        });
+        })->orderBy('created_at', 'desc');
 
         // Map column indexes to column names (modify these based on your actual database structure)
         $columnNames = ['date', 'number_result', 'project_name'];
