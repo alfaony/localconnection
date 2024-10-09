005f67b36354c36f17351e0496dc6747410a9277
AlfaonyThrive <alfaony@thrive.co.id>
2024-10-09 11:23:09 +0700

    add With Trash

--- app/Models/Bast.php
+++ app/Models/Bast.php
@@ -66,12 +66,12 @@ class Bast extends Model
 
     public function workOrder()
     {
-        return $this->belongsTo(WorkOrder::class);
+        return $this->belongsTo(WorkOrder::class)->withTrashed();
     }
 
     public function project()
     {
-        return $this->belongsTo(Project::class);
+        return $this->belongsTo(Project::class)->withTrashed();
     }
 
     public function invoice()
