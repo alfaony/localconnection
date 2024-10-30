24edb92ac855235eb16b0a898fae33f92fc005fe
AlfaonyThrive <alfaony@thrive.co.id>
2024-10-30 12:41:32 +0700

    improvement bast email record

--- database/migrations/2024_10_29_120739_create_email_records_table.php
+++ database/migrations/2024_10_29_120739_create_email_records_table.php
@@ -16,12 +16,14 @@ return new class extends Migration
         Schema::create('bast_email_records', function (Blueprint $table) {
             $table->id();
             $table->uuid('bast_id');
+            $table->uuid('user_id')->nullable();
             $table->json('to'); // Store as JSON
             $table->json('cc')->nullable(); // Store as JSON
             $table->string('subject')->nullable();
             $table->text('content')->nullable();
             $table->timestamps();
 
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->foreign('bast_id')->references('id')->on('basts')->onDelete('cascade');
         });
     }
