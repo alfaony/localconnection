4ed561bbb0730a3c9f15456ef992fdb9e7098f8b
AlfaonyThrive <alfaony@thrive.co.id>
2024-11-07 10:08:43 +0700

    getItems

--- app/Console/Commands/CheckinNotificationV2.php
+++ app/Console/Commands/CheckinNotificationV2.php
@@ -123,7 +123,7 @@ class CheckinNotificationV2 extends Command
             if ($sendReport->hasFailures()) 
             {
                 // Log or handle any failures
-                foreach ($sendReport->failures() as $failure) {
+                foreach ($sendReport->failures()->getItems() as $failure) {
                     Log::error('Notification failed for token: ' . $failure->target()->value());
                 }
             }
