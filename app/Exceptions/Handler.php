657708b3540ef2df802fe0aa1294931b72226df3
AlfaonyThrive <alfaony@thrive.co.id>
2024-11-12 09:21:32 +0700

    remove dd

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -62,7 +62,6 @@ class Handler extends ExceptionHandler
     // discrod
     public function discordLog($data)
     {
-        dd($data);
         $status_colors = [
             'emergency' => '15548997',
             'alert' => '15418782',
