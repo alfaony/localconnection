71f2583976256f98b04e5cc701925f38237e5f54
AlfaonyThrive <alfaony@thrive.co.id>
2024-12-06 15:49:40 +0700

    Update

--- app/Http/Controllers/KyeController.php
+++ app/Http/Controllers/KyeController.php
@@ -372,7 +372,7 @@ class KyeController extends Controller
         
         // dd($ccEmails);
         // Email Helper Notification
-         dd(EmailNotifHelper::sentEmail(
+         EmailNotifHelper::sentEmail(
             $fromEmail,
             $fromName,
             $toEmails, 
@@ -383,7 +383,7 @@ class KyeController extends Controller
             $smtpConfig, 
             $companyId, 
             $ccEmails
-        ));
+        );
     }
 
     private function sentInbox($to,$message,$directUrl)
