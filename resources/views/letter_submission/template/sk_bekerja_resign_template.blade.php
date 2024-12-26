e3fc97a2430e74cacf5bda21cab18e7478ed5655
AlfaonyThrive <alfaony@thrive.co.id>
2024-12-26 20:32:33 +0700

    approvement

--- resources/views/letter_submission/template/sk_bekerja_resign_template.blade.php
+++ resources/views/letter_submission/template/sk_bekerja_resign_template.blade.php
@@ -124,6 +124,8 @@
 @endsection
 
 @section('js')
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"></script>
 <script>
 $(document).ready(function() {
     $("#downloadQuote").click(function(e) {
