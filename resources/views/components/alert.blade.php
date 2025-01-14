a05ef5d07378fad4ed4969feffd26dbbab9b9962
AlfaonyThrive <alfaony@thrive.co.id>
2025-01-14 14:03:22 +0700

    Update

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -2,21 +2,21 @@
 @if(Session::has('store'))
 <div class="alert alert-success alert-dismissible fade show mt-3" role="alert">
     <strong>Berhasil!</strong> Data berhasil ditambahkan.
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+
 </div>
 @endif
 
 @if(Session::has('update'))
 <div class="alert alert-success alert-dismissible fade show mt-3" role="alert">
     <strong>Berhasil!</strong> Data berhasil diperbarui.
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+
 </div>
 @endif
 
 @if(Session::has('delete'))
 <div class="alert alert-danger alert-dismissible fade show mt-3" role="alert">
     <strong>Berhasil!</strong> Data berhasil dihapus.
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+
 </div>
 @endif
 
@@ -28,6 +28,6 @@
         <li>{{ $error }}</li>
         @endforeach
     </ul>
-    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+
 </div>
 @endif
