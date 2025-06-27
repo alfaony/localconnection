5b3b622621d7e80963c28329e095c42f11d69e21
AlfaonyThrive <alfaony@thrive.co.id>
2025-06-27 22:34:42 +0700

    add

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,7 @@ class Kernel extends ConsoleKernel
 
         // Tetapkan zona waktu Asia/Jakarta
         // Jadwalkan pekerjaan 'project:reccuring' setiap hari pada pukul 00:00
-        $schedule->command('project:reccuring')->timezone('Asia/Jakarta')->dailyAt('00:00');
+        // $schedule->command('project:reccuring')->timezone('Asia/Jakarta')->dailyAt('00:00');
         $schedule->command('project:set-status-sent-time')->timezone('Asia/Jakarta')->dailyAt('00:00');
         $schedule->command('tasks:process-recurring')->timezone('Asia/Jakarta')->dailyAt('00:00');
         $schedule->command('recurring:generate')->timezone('Asia/Jakarta')->dailyAt('01:00');
