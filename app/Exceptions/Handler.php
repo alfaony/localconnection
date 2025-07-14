679fc665c566d96e8127173c71adfd4068df913f
AlfaonyThrive <alfaony@thrive.co.id>
2025-07-14 14:25:51 +0700

    add

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Http\Exceptions\PostTooLargeException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Support\Facades\Http;
+use Sentry\Laravel\Integration as SentryIntegration;
 
 use Throwable;
 
@@ -49,6 +50,11 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
+
+            if (app()->bound('sentry')) {
+                dd($e);
+                Sentry\captureException($e);
+            }
             $data = [
                 'content' => $e->getMessage(),
                 'title' => 'Line: '.$e->getLine(),
