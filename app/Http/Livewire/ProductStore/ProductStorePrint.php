2b55ed78363e55c42c8409ae134b907d3471e357
AlfaonyThrive <alfaony@thrive.co.id>
2025-09-24 09:38:15 +0700

    add user

--- app/Http/Livewire/ProductStore/ProductStorePrint.php
+++ app/Http/Livewire/ProductStore/ProductStorePrint.php
@@ -45,12 +45,22 @@ class ProductStorePrint extends Component
                     // kecil, biar gampang di-scale via CSS
                     $barcodeSvg = DNS2D::getBarcodeSVG($product->barcode, 'QRCODE', $widthCode, $heightCode);
                 } else {
+                    // DNS1D::setStorePath(null); // Optional, reset cache path
+                    // ob_clean(); // Optional: flush output buffer, jaga-jaga
+
                     $barcodeType = $this->barcodeType === 'CODE128' ? 'C128' : 'C39';
-                    $barcodeSvg = DNS1D::getBarcodeSVG($product->barcode, $barcodeType, $code - 5, $code);
+                    $barcodeSvg = DNS1D::getBarcodeSVG($product->barcode, $barcodeType, 1, $code);
+
+                    // Step 1: Hapus semua <text> bawaan (prevent duplikat)
+                    $barcodeSvg = preg_replace('/<text.*?<\/text>/', '', $barcodeSvg);
+
+                    // Step 2: Sisipkan text baru sebelum tag penutup </svg>
+                    $barcodeText = '<text x="' . ($this->barcodeType === 'CODE128' ? '61.5' : '120') . '" text-anchor="middle" y="44.2" id="code" fill="black" font-size="12px">' . $product->barcode . '</text>';
+                    $barcodeSvg = str_replace('</svg>', $barcodeText . '</svg>', $barcodeSvg);
                 }
 
                 // Inject properti preserveAspectRatio ke SVG supaya scalable
-                $barcodeSvg = str_replace('<svg', '<svg preserveAspectRatio="xMidYMid meet"', $barcodeSvg);
+                // $barcodeSvg = str_replace('<svg', '<svg preserveAspectRatio="xMidYMid meet"', $barcodeSvg);
                 
                 $this->barcodePreviews[] = [
                     'id' => $product->id,
