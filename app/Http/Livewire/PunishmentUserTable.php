fa14e1741d5a059b4b8bd1feb68b04f910364fde
AlfaonyThrive <alfaony@thrive.co.id>
2025-09-25 18:15:52 +0700

    bugfixing

--- app/Http/Livewire/PunishmentUserTable.php
+++ app/Http/Livewire/PunishmentUserTable.php
@@ -55,7 +55,7 @@ class PunishmentUserTable extends Component
     public function render()
     {
         // Get users by company
-        $companyUsers = User::byCompany(Auth::user())->get();
+        $companyUsers = User::byCompany(Auth::user()->company_id)->get();
         
         $punishments = PunishmentUser::with(['user', 'dailytask'])->byCompany(Auth::user()->company_id)
             ->when($this->selectedUser, function($query) {
