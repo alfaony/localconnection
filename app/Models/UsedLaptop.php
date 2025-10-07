2c74bf8472fa1712cb90d1d515f4d13d46dedf22
AlfaonyThrive <alfaony@thrive.co.id>
2025-10-07 10:05:43 +0700

    add

--- app/Models/UsedLaptop.php
+++ app/Models/UsedLaptop.php
@@ -90,7 +90,7 @@ class UsedLaptop extends Model
     public function getJambiPriceAttribute()
     {
 
-        return $this->suggested_selling_price + $this->jakarta_price + config('services.used_laptop_charge.totebag_charge') + config('services.used_laptop_charge.expedition_charge');
+        return $this->jakarta_price + config('services.used_laptop_charge.expedition_charge');
     }
 
     // 🟢 Status Jual (label helper opsional)
