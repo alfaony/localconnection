1b7c5650fc3e34ede2ff18a3cd4ade1fa6a8b464
AlfaonyThrive <alfaony@thrive.co.id>
2026-01-08 14:57:37 +0700

    add

--- resources/views/report_project/show.blade.php
+++ resources/views/report_project/show.blade.php
@@ -119,7 +119,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="noteModalLabel">Catatan Approvement</h5>
+                <h5 class="modal-title" id="noteModalLabel">Catatan Penolakan</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
