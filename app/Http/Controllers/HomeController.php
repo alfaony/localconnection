8bb6d15528e2775b0c64711d256b1ee0a2d6d42d
AlfaonyThrive <alfaony@thrive.co.id>
2026-02-26 13:50:41 +0700

    done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -736,7 +736,7 @@ class HomeController extends Controller
                         'nama' => $sub->package->nama_paket,
                     ],
                     'tanggal_expired' => Carbon::parse($sub->tanggal_expired)->format('d M Y'),
-                    'software_url' => route('customer-software.show', $sub->software->id),
+                    'software_url' => route('customer-software.show', $sub->software->slug),
                 ];
             });
 
