395a8b0b41ced23747d214d991b057b692298c04
AlfaonyThrive <alfaony@thrive.co.id>
2026-03-18 16:43:57 +0700

    add asset Type

--- app/Models/AssetType.php
+++ app/Models/AssetType.php
@@ -19,9 +19,10 @@ class AssetType extends Model
     {
         if($companyId)
         {
-            return $query->whereHas('user', function ($query) use ($companyId) 
+            $companyIds = auth()->user()->accessibleCompanies->pluck('id')->push($companyId)->unique();
+            return $query->whereHas('user', function ($query) use ($companyIds) 
             {
-                $query->where('company_id', $companyId);
+                $query->whereIn('company_id', $companyIds);
             });
         }
     }
