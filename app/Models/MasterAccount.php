00f01fd3e703f9ff7392dbbf75416aa737251553
AlfaonyThrive <alfaony@thrive.co.id>
2026-03-25 09:40:32 +0700

    Fast Bug

--- app/Models/MasterAccount.php
+++ app/Models/MasterAccount.php
@@ -200,7 +200,8 @@ class MasterAccount extends Model
      */
     public function scopeByCompany($query, $companyId)
     {
-        return $query->where('company_id', $companyId);
+         $companyIds = auth()->user()->accessibleCompanies->pluck('id')->push($companyId)->unique();
+        return $query->whereIn('company_id', $companyIds);
     }
 
     /**
