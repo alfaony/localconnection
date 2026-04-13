aa68ea77659d37abee3dba3233dfbec33d1a38a1
AlfaonyThrive <alfaony@thrive.co.id>
2026-04-13 22:40:47 +0700

    add

--- app/Http/Controllers/DailyTaskController.php
+++ app/Http/Controllers/DailyTaskController.php
@@ -587,13 +587,17 @@ class DailyTaskController extends Controller
             }
             if($dailyTask->assignment_user_id != $request->assignment_user_id)
             {
-                $message = "Mengubah Tugas ".$dailyTask->name." dari ".User::find($dailyTask->assignment_user_id)->name." menjadi ".User::find($request->assignment_user_id)->name;
+                $from = User::find($dailyTask->assignment_user_id) ? User::find($dailyTask->assignment_user_id)->name : "";
+                $to = User::find($request->assignment_user_id) ? User::find($request->assignment_user_id)->name : "";
+                $message = "Mengubah Tugas ".$dailyTask->name." dari ".$from." menjadi ".$to;
             }
             elseif ($dailyTask->end_date != $request->end_date) {
-                $message = "Mengubah Tugas ".$dailyTask->name." dari ".Carbon::parse($dailyTask->end_date)->format('d-m-Y')." menjadi ".Carbon::parse($request->end_date)->format('d-m-Y');
+                $from = Carbon::parse($dailyTask->end_date)->format('d-m-Y');
+                $to = Carbon::parse($request->end_date)->format('d-m-Y');
+                $message = "Mengubah Tugas ".$dailyTask->name." dari ".$from." menjadi ".$to;
             }
         
-            if($message)
+            if($message )
             {
                 $directUrl = route('dailytask.show', ['dailytask' => $dailyTask->slug]);
                 if(Auth::user()->id == $request->assignment_user_id)
@@ -608,8 +612,10 @@ class DailyTaskController extends Controller
                     $userTo = $request->assignment_user_id;
                     $this->sentInbox($dailyTask->user_id,$message, $directUrl);
                 }
-    
-                $this->sentInbox($userTo,$message, $directUrl);
+                
+                if($userTo){
+                    $this->sentInbox($userTo,$message, $directUrl);
+                }
             }  
             if($request->point > 0)
             {
@@ -761,7 +767,7 @@ class DailyTaskController extends Controller
             DB::commit();
             return redirect()->route('dailytask.index')->with('update', true);
         } catch (\Throwable $th) {
-            // dd($th); 
+            dd($th); 
             DB::rollback();
             Log::error($th->getMessage());
 
